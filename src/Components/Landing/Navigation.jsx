b25cd6fff5764ba1c9f012b468593a491525828d
eralmendral <eralmendral@gmail.com>
2021-06-02 17:24:52 +0800

    fix: navigation responsive styling

--- src/Components/Landing/Navigation.jsx
+++ src/Components/Landing/Navigation.jsx
@@ -6,14 +6,15 @@ import './Navigation.scss';
 
 function Navigation() {
   return (
-    <div className="navigation">
-      <div className="dot-container">
-        <div className="vertical-line"></div>
-        <div className="dot"></div>
-        <div className="dot"></div>
-      </div>
+    <div className="navigation-container">
+      <div className="navigation">
+        <div className="dot-container">
+          <div className="vertical-line"></div>
+          <div className="dot"></div>
+          <div className="dot"></div>
+        </div>
 
-      <div className="navigation-links">
+        <div className="navigation-links">
         <h2>
           <AnchorLink className="nav-link" href='#projects'>Projects</AnchorLink>
         </h2>
@@ -21,6 +22,7 @@ function Navigation() {
           <AnchorLink className="nav-link" href='#experience'>Experience</AnchorLink>
         </h2>
         <h2>  <AnchorLink className="nav-link" href='#skills'>Skills</AnchorLink></h2>
+      </div>
       </div>
     </div>
   )
